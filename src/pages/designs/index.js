fa3dba4cb9a75d1436a48f3a00728a8707656a3d
Evan Winter <ewinter@sccadv.com>
2019-07-29 23:23:10 -0500

    use Porfolio instead of pulling all Projects so we can reorder in the CMS

--- src/pages/designs/index.js
+++ src/pages/designs/index.js
@@ -5,10 +5,11 @@ import SEO from "../../components/seo"
 import { getSlug } from "../../utils"
 
 const DesignsPage = ({ data }) => {
-	const projects = data.allContentfulProjects.edges.map((edge) => edge.node)
+	const { projects } = data.allContentfulPortfolio.edges.map(edge => edge.node)[0]
 	return (
 		<Fragment>
-			<SEO title="Designs" keywords={["emily", "buchberger", "design", "portfolio"]} />
+			<SEO title="Designs" keywords={["emily", "buchberger", "design", "portfolio"]}
+			/>
 			<section className="portfolio-page">
 				<div className="grid two">
 					{projects.map(project => (
@@ -16,7 +17,9 @@ const DesignsPage = ({ data }) => {
 							<Img fluid={project.coverImage.localFile.childImageSharp.fluid} />
 							<div className="project-reveal">
 								<div className="project-details">
-									<h1 className="title">{project.title} <span className="emdash">&mdash;</span></h1>
+									<h1 className="title">
+										{project.title} <span className="emdash">&mdash;</span>
+									</h1>
 									<h3 className="subtitle">{project.subtitle}</h3>
 								</div>
 							</div>
@@ -32,25 +35,30 @@ export default DesignsPage
 
 export const query = graphql`
 	query designsQuery {
-	  allContentfulProjects {
-	    edges {
-	      node {
-	        id
-	        title
-	        slug
-	        subtitle
-	        coverImage {
-	        	title
-	        	localFile {
-	        		childImageSharp {
-	        			fluid(maxWidth: 1440) {
-	        				...GatsbyImageSharpFluid_noBase64
-	        			}
-	        		}
-	        	}
-	        }
-	      }
-	    }
-	  }
+		allContentfulPortfolio {
+			edges {
+				node {
+					title
+					projects {
+						... on ContentfulProjects {
+							id
+							slug
+							subtitle
+							title
+							coverImage {
+								title
+								localFile {
+									childImageSharp {
+										fluid(maxWidth: 1440) {
+											...GatsbyImageSharpFluid_noBase64
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 `
