13deae6a14acc7e9a3ae0fe7e43a409da7d3a56e
Evan Winter <ewinter1996@gmail.com>
2019-09-09 22:39:15 -0500

    remove "students" from bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,7 @@ const AboutPage = () => (
 				</div>
 				<div className="right">
 					<p>
-						Welcome! I'm Emily Buchberger, a designer and student in Chicago.
+						Welcome! I'm Emily Buchberger, a designer in Chicago.
 						I'm an ENFP according to the Myers-Briggs test and an Aries
 						according to the stars. I, of course, do not subscribe to any labels
 						and would love to get to know you more personally.
